c905368b59119f7f30d872118a66a3227fec40c6
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-03-08 14:11:16 +0300

    start to add & update current category

--- src/app/modules/categories/components/add-new-category/add-new-category.component.ts
+++ src/app/modules/categories/components/add-new-category/add-new-category.component.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
+import {RouterPathsEnum} from "../../../../shared/enums/routerPaths.enum";
 
 @Component({
       selector: "crm-add-new-category",
       templateUrl: "./add-new-category.component.html",
       styleUrls: ["./add-new-category.component.css"],
 })
-export class AddNewCategoryComponent {}
+export class AddNewCategoryComponent {
+  routerPathsEnum = RouterPathsEnum;
+}
